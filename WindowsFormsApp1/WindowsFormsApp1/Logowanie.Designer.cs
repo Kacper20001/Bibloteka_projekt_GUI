7d38461681efb40652622a1657ede5cf5416e200
Kacper Kulig <kuligkacper20001@gmail.com>
2024-01-10 17:54:08 +0100

    Logowanie

--- WindowsFormsApp1/WindowsFormsApp1/Logowanie.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Logowanie.Designer.cs
@@ -29,23 +29,74 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LogForgotPassword = new System.Windows.Forms.Label();
+            this.logSubmit = new System.Windows.Forms.Button();
+            this.logLogin = new System.Windows.Forms.TextBox();
+            this.logPassword = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(282, 34);
+            this.label1.Location = new System.Drawing.Point(44, 41);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // LogForgotPassword
+            // 
+            this.LogForgotPassword.AutoSize = true;
+            this.LogForgotPassword.Location = new System.Drawing.Point(44, 133);
+            this.LogForgotPassword.Name = "LogForgotPassword";
+            this.LogForgotPassword.Size = new System.Drawing.Size(104, 13);
+            this.LogForgotPassword.TabIndex = 2;
+            this.LogForgotPassword.Text = "I forgot my password";
+            // 
+            // logSubmit
+            // 
+            this.logSubmit.Location = new System.Drawing.Point(114, 175);
+            this.logSubmit.Name = "logSubmit";
+            this.logSubmit.Size = new System.Drawing.Size(75, 23);
+            this.logSubmit.TabIndex = 3;
+            this.logSubmit.Text = "Submit";
+            this.logSubmit.UseVisualStyleBackColor = true;
+            this.logSubmit.Click += new System.EventHandler(this.logSubmit_Click);
+            // 
+            // logLogin
+            // 
+            this.logLogin.Location = new System.Drawing.Point(114, 41);
+            this.logLogin.Name = "logLogin";
+            this.logLogin.Size = new System.Drawing.Size(100, 20);
+            this.logLogin.TabIndex = 4;
+            // 
+            // logPassword
+            // 
+            this.logPassword.Location = new System.Drawing.Point(114, 87);
+            this.logPassword.Name = "logPassword";
+            this.logPassword.Size = new System.Drawing.Size(100, 20);
+            this.logPassword.TabIndex = 5;
             // 
             // Logowanie
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(250, 246);
+            this.Controls.Add(this.logPassword);
+            this.Controls.Add(this.logLogin);
+            this.Controls.Add(this.logSubmit);
+            this.Controls.Add(this.LogForgotPassword);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Logowanie";
             this.Text = "Logowanie";
@@ -57,5 +108,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LogForgotPassword;
+        private System.Windows.Forms.Button logSubmit;
+        private System.Windows.Forms.TextBox logLogin;
+        private System.Windows.Forms.TextBox logPassword;
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/Logowanie.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Logowanie.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApp1
 {
@@ -17,9 +19,71 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void logSubmit_Click(object sender, EventArgs e)
         {
+            string username = logLogin.Text;
+            string password = logPassword.Text;
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                string passwordHash = GetHash(sha256Hash, password);
+                Reader reader = GetReader(username, passwordHash);
 
+                if (reader != null)
+                {
+                    MessageBox.Show("Zalogowano pomyślnie.");
+                    /*ReaderMenu readerMenu = new ReaderMenu(reader);
+                    readerMenu.Show();
+                    this.Hide();*/
+                }
+                else
+                {
+                    MessageBox.Show("Błąd logowania. Sprawdź dane logowania.");
+                }
+            }
+        }
+
+        internal Reader GetReader(string username, string passwordHash)
+        {
+            Reader foundReader = null;
+            using (SqlConnection connection = new SqlConnection("Data Source=DESKTOP-3QM33ET\\SQLEXPRESS;InitialCatalog=LibraryDB;Integrated Security=True"))
+            {
+                string query = "SELECT * FROM Readers WHERE Username = @Username AND Password = @Password";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", username);
+                    command.Parameters.AddWithValue("@Password", passwordHash);
+
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            foundReader = new Reader(
+                                reader["FirstName"].ToString(), reader["LastName"].ToString(),
+                                Convert.ToDateTime(reader["DateOfBirth"]), reader["PhoneNumber"].ToString(),
+                                reader["Email"].ToString(),
+                                new Address(reader["Street"].ToString(), reader["HouseNumber"].ToString(),
+                                reader["PostalCode"].ToString(), reader["City"].ToString(),
+                                reader["Country"].ToString()),
+                                reader["Password"].ToString(), reader["Username"].ToString());
+                        }
+                    }
+                }
+            }
+            return foundReader;
+        }
+
+        static string GetHash(HashAlgorithm hashAlgorithm, string input)
+        {
+            byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            StringBuilder sBuilder = new StringBuilder();
+
+            foreach (byte b in data)
+            {
+                sBuilder.Append(b.ToString("x2"));
+            }
+            return sBuilder.ToString();
         }
     }
 }
